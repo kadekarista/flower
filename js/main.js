6586f8eab22f275193ae40c82e8d4ef4e60affdc
kadekarista <156432936+kadekarista@users.noreply.github.com>
2024-12-14 18:35:20 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('NAILA').split('CANTIK')
+    const titles = ('NAILA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
